266a6e2e2685c6001e351aa35988526bee1f2f97
ssoranaokil <naoki6117@gmail.com>
2023-04-16 13:42:26 +0900

    !つけた

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,7 +24,7 @@
         <div class="form-group">
             <div class="col-sm-offset-3 col-sm-6">
                 <button type="submit" class="btn btn-default">
-                    <i class="fa fa-plus"></i> Add Task
+                    <i class="fa fa-plus"></i> Add Task!
                 </button>
             </div>
         </div>
